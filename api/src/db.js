6ebdd447f43f1a57fb46ed84af3a29c23af79f1b
wickgit <96148289+wickgit@users.noreply.github.com>
2026-07-23 17:37:06 +0000

    fix(api): support running migrations inside the container

--- api/src/db.js
+++ api/src/db.js
@@ -2,6 +2,6 @@ import 'dotenv/config';
 import knex from 'knex';
 import configs from '../knexfile.js';
 
-const environment = process.env.NODE_ENV === 'test' ? 'test' : 'development';
+const environment = configs[process.env.NODE_ENV] ? process.env.NODE_ENV : 'development';
 
 export const db = knex(configs[environment]);
